87dbebeb6ed1812da0e98f05c7c3fa8168e3049c
munshyn <husny.sk@gmail.com>
2024-01-14 00:54:17 +0800

    added bg-music

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,19 @@ import HelloWorld from './components/HelloWorld.vue'
 
 <template>
     <img alt="Vue logo" class="block m-auto" src="@/assets/animel-icon-big.png" width="125" height="125" />
-
+    <div class="music-player">
+      <audio
+        ref="audio"
+        src="@/assets/bg-music.mp3"
+        autoplay
+        loop
+        id="audio"
+      ></audio>
+      <audio>
+        <source src="@/assets/bg-music.mp3" type="audio/mpeg">
+      </audio>
+    <div @click="toggleSound()" class="toggle-sound"></div>
+  </div>
     <div class="flex justify-center">
       <div class="bg-white text-black w-1/3 flex justify-between">
         <div class="self-center text-2xl nes-text">
